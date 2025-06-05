f209f7888398a48e76d78c47366e38968ce84160
Chris Waycott <chriswaycott@gmail.com>
2025-06-05 12:11:03 -0700

    feat(ui): overlay Tailwind button above Phaser canvas

--- src/main.js
+++ src/main.js
@@ -1,16 +1,15 @@
+import "./index.css";
 import Phaser from 'phaser';
 import BootScene from './scenes/BootScene.js';
 import GameScene from './scenes/GameScene.js';
 
 const config = {
   type: Phaser.AUTO,
-  parent: 'game',
-  width: 800,
-  height: 600,
-  backgroundColor: '#2d2d2d',
+  width: 640,
+  height: 640,
+  parent: "phaser-game",        // Phaser will insert its <canvas> here
   scene: [BootScene, GameScene],
-  pixelArt: true,
-  physics: { default: 'arcade' }
+  // …other config options
 };
 
-window.game = new Phaser.Game(config);
+new Phaser.Game(config);
