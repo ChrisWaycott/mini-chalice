366cbcb5fa8bdf922ad4a5d752ddc7e0b78fc3e9
Chris Waycott <chriswaycott@gmail.com>
2025-06-05 08:41:12 -0700

    fix: fit to tile again

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -29,14 +29,14 @@ export default class GameScene extends Phaser.Scene {
       .setOrigin(0.5, 1)
       .play('raider-idle');
     scaleToTile(this.player);
-    this.player.y += 120 * this.player.scaleY;
+    this.player.y += 60 * this.player.scaleY;
     this.player.setScale(this.player.scaleX * 2);
     this.player.gridX = 0;
     this.player.gridY = 0;
     this.player.alive = true;
 
     /* shadow */
-    this.shadow = this.add.ellipse(this.player.x, this.player.y + 20, 36, 16, 0x000000, 0.3);
+    this.shadow = this.add.ellipse(this.player.x, this.player.y + 40, 36, 16, 0x000000, 0.3);
 
     /* ---------- fog-of-war ---------- */
     this.fogRT       = this.make.renderTexture({ width: 640, height: 640, add: true });
